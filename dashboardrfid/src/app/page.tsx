8cfc3743e0ebc6a56b24a923e828733747c83b8b
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-03-27 16:16:02 +0100

    design sytem started

--- dashboardrfid/src/app/page.tsx
+++ dashboardrfid/src/app/page.tsx
@@ -1,6 +1,40 @@
+import { Typographie } from "@/ui/design-system/typographie/Typogarphie";
 
 export default function Home() {
   return (
-    <h1>salut</h1>
+    <>
+      <div className="flex flex-col ">
+        <Typographie variant="display">
+            Typographie
+        </Typographie>
+        <Typographie variant="h2">
+            Typographie
+        </Typographie>
+        <Typographie variant="h3">
+            Typographie
+        </Typographie>
+        <Typographie variant="subtitle">
+            Typographie
+        </Typographie>
+        <Typographie variant="body-base">
+            Typographie
+        </Typographie>
+        <Typographie variant="body-bold">
+            Typographie
+        </Typographie>
+        <Typographie variant="body-sm">
+            Typographie
+        </Typographie>
+        <Typographie variant="pretitle">
+            Typographie
+        </Typographie>
+        <Typographie variant="button-text">
+            Typographie
+        </Typographie>
+        <Typographie variant="link">
+            Typographie
+        </Typographie>
+      </div>
+    </>
   );
 }
--- dashboardrfid/tailwind.config.ts
+++ dashboardrfid/tailwind.config.ts
@@ -7,13 +7,78 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontSize: {
+      "4xl": [
+        "64px",
+        {
+          fontWeight: "700",
+          letterSpacing: "-2%",
+        },
+      ],
+      "3xl": [
+        "40px",
+        {
+          fontWeight: "700",
+          letterSpacing: "-2%",
+        },
+      ],
+      "2xl": [
+        "24px",
+        {
+          fontWeight: "700",
+          letterSpacing: "-2%",
+        },
+      ],
+      xl: [
+        "24px",
+        {
+          fontWeight: "500",
+        },
+      ],
+      body: [
+        "16px",
+        {
+          fontWeight: "500",
+          letterSpacing: "-2%",
+          lineHeight: "140%"
+        },
+      ],
+      bold: [
+        "16px",
+        {
+          fontWeight: "700",
+        
+        },
+      ],
+      small: [
+        "14px",
+        {
+          fontWeight: "500",
+        },
+      ],
+      pretitle: [
+        "10px",
+        {
+          fontWeight: "700",
+          letterSpacing: "3%",
+        },
+      ],
+      button: [
+        "16px",
+        {
+          fontWeight: "700",
+          letterSpacing: "3%",
+        },
+      ],
+      link: [
+        "16px",
+        {
+          fontWeight: "700",
+          letterSpacing: "3%",
+        },
+      ],
     },
+    extend: {},
   },
   plugins: [],
 };
