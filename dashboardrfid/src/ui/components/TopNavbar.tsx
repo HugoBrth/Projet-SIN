e55d3cb3048a2cdda42bf21a3b056a7a19a1edf2
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-05-15 16:39:16 +0200

    done

--- dashboardrfid/src/ui/components/TopNavbar.tsx
+++ dashboardrfid/src/ui/components/TopNavbar.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 const TopNavbar = () => {
   return (
     <div className=" ml-[130px] flex items-center px-4 py-2 justify-between">
-      <form className="flex items-center w-[200px] bg-[#323232] rounded-xl px-4 py-3 gap-2">
+      <form className="flex items-center max-w-[250px] bg-[#323232] rounded-xl px-4 py-3 gap-2">
         <IoIosSearch color="white" size={20} />
         <input
           className="text-small w-full bg-transparent border-none outline-none text-white font-normal"
